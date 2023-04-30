f19fb5828e6a6617abc0404e2d0a6b75d916ad93
Madhavi Sapkota <madhavi.sapkota@gmail.com>
2023-04-29 21:31:53 -0500

    user-data structure done

--- tic-tac-toe/src/app/app.module.ts
+++ tic-tac-toe/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TicTacToeGridComponent } from './tic-tac-toe-grid/tic-tac-toe-grid.component';
 import { CellComponent } from './tic-tac-toe-grid/cell/cell.component';
+import { UsersComponent } from './tic-tac-toe-grid/users/users.component';
 
 @NgModule({
-  declarations: [AppComponent, TicTacToeGridComponent, CellComponent],
+  declarations: [AppComponent, TicTacToeGridComponent, CellComponent, UsersComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
